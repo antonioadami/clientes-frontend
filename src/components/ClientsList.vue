445573ef19243a696109aefe04fe4f5795346f4a
Antonio <antonioadamipalma@gmail.com>
2021-08-14 16:48:49 -0300

    Implementando element-ui e table de clientes

--- src/components/ClientsList.vue
+++ src/components/ClientsList.vue
@@ -1,12 +1,33 @@
 <template>
   <div class="page">
     <div class="clients-list">
-      <h1>Clientes</h1>
-      <ul>
-        <li v-for="client in clients" :key="client.cpf">
+      <div class="header-container">
+        <h1>Clientes</h1>
+        <el-button type="primary" icon="el-icon-plus">Adicionar Cliente</el-button>
+      </div>
+      <table>
+        <th>
+          <td>Nome</td>
+          <td>E-mail</td>
+          <td>Telefone</td>
+          <td>Idade</td>
+          <td>CPF</td>
+          <td id="delete-client-container">Ações</td>
+        </th>
+        <tr class="client-container" v-for="client in clients" :key="client.cpf">
+          <td class="client-data-container">{{client.name}}</td>
+          <td class="client-data-container">{{client.email}}</td>
+          <td class="client-data-container">{{client.phone}}</td>
+          <td class="client-data-container">{{client.age}}</td>
+          <td class="client-data-container">{{client.cpf}}</td>
+          <td class="client-data-container" id="delete-client-container">
+            <el-button type="danger" icon="el-icon-delete" circle></el-button>
+          </td>
+        </tr>
+      </table>
+        <!-- <li v-for="client in clients" :key="client.cpf">
           <div class="client-container">
             <div class="client-data-container">
-              <h6>Nome</h6>
               <p>{{client.name}}</p>
             </div>
             <div class="client-data-container">
@@ -18,12 +39,18 @@
               <p>{{client.phone}}</p>
             </div>
             <div class="client-data-container">
+              <h6>Idade</h6>
+              <p>{{client.age}}</p>
+            </div>
+            <div class="client-data-container">
               <h6>CPF</h6>
               <p>{{client.cpf}}</p>
             </div>
+            <div class="client-data-container" id="delete-client-container">
+              <el-button type="danger" icon="el-icon-delete" circle></el-button>
+            </div>
           </div>
-        </li>
-      </ul>
+        </li> -->
     </div>
   </div>
 </template>
@@ -74,15 +101,23 @@
     -moz-box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.5);
   }
 
-  h1 {
-    font-size: 32px;
-    font-weight: 600;
+  .header-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
     margin-bottom: 1rem;
   }
 
-  ul {
+  h1 {
+    font-size: 32px;
+    font-weight: 600;
+  }
+
+  table {
     list-style-type: none;
-    
+    border: 1px solid #bbbbbb;
+    border-radius: 5px;
 
     display: flex;
     justify-content: center;
@@ -91,7 +126,7 @@
 
   }
 
-  li {
+  tr, th {
     display: flex;
     justify-content: center;
     align-items: center;
@@ -99,18 +134,8 @@
     width: 100%;
   }
 
-  li:nth-child(even) {
+  tr:nth-child(even) {
     background-color: #f2f2f2;
-  }
-
-  h6 {
-    margin: 0 0 1rem 0;
-    font-size: 16px;
-    font-weight: 600;
-  }
-
-  p {
-    font-size: 14px;
   }
   
   .client-container {
@@ -119,7 +144,7 @@
     align-items: center;
   }
 
-  .client-data-container {
+  td {
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -127,10 +152,17 @@
 
     width: 170px;
     padding: 1rem;
+    font-size: 14px;
   }
 
-  .client-data-container:not(:nth-last-child(1)) {
-    margin-right: 1rem;
+  th td {
+    font-size: 16px;
+    font-weight: 600;
+  }
+
+  #delete-client-container {
+    align-items: flex-end;
+    width: auto;
   }
 
 </style>
